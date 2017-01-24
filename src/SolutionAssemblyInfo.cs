92d148076cc11b4cb2428547614db8d710d4bd24
Geert van Horrik <geert@catenalogic.com>
2017-01-24 11:39:01 +0100

    Update SolutionAssemblyInfo

--- src/SolutionAssemblyInfo.cs
+++ src/SolutionAssemblyInfo.cs
@@ -8,10 +8,27 @@
 using System.Reflection;
 using System.Resources;
 
+// Shared assembly info that is common for all assemblies of this project
+
+////[assembly: AssemblyTitle("DEFINED IN ACTUAL ASSEMBLYINFO")]
+////[assembly: AssemblyProduct("DEFINED IN ACTUAL ASSEMBLYINFO")]
+////[assembly: AssemblyDescription("DEFINED IN ACTUAL ASSEMBLYINFO")]
+
 [assembly: AssemblyCompany("WildGums")]
 [assembly: AssemblyCopyright("Copyright © WildGums 2016 - 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+
 [assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0, Nightly, Released on 2013-09-01 13:32")]
