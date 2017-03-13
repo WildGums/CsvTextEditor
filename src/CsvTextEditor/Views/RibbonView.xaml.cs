6856d6b180f754dce054d967d186e174f8279973
Geert van Horrik <geert@catenalogic.com>
2017-03-13 22:06:06 +0100

    Add about button to ribbon

--- src/CsvTextEditor/Views/RibbonView.xaml.cs
+++ src/CsvTextEditor/Views/RibbonView.xaml.cs
@@ -7,12 +7,16 @@
 
 namespace CsvTextEditor.Views
 {
+    using Orchestra;
+
     public partial class RibbonView
     {
         #region Constructors
         public RibbonView()
         {
             InitializeComponent();
+
+            ribbon.AddAboutButton();
         }
         #endregion
     }
