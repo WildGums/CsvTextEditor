a25927f34acb62a5d40711d339ae0dcb4b84b232
Geert van Horrik <geert@catenalogic.com>
2017-03-13 22:05:56 +0100

    Add new logo

---
+++ design/Create images.bat
@@ -0,0 +1,11 @@
+@REM Windows Store
+
+set iconFile=logo\logo.svg
+
+inkscape.exe %iconFile% --export-png=logo\logo_0032.png -w32 -h32
+inkscape.exe %iconFile% --export-png=logo\logo_0055.png -w55 -h55
+inkscape.exe %iconFile% --export-png=logo\logo_0064.png -w64 -h64
+inkscape.exe %iconFile% --export-png=logo\logo_0128.png -w128 -h128
+inkscape.exe %iconFile% --export-png=logo\logo_0256.png -w256 -h256
+inkscape.exe %iconFile% --export-png=logo\logo_0512.png -w512 -h512
+inkscape.exe %iconFile% --export-png=logo\logo_1024.png -w1024 -h1024
