d8b9ff8b688fdb2be74be957adb854c90044a926
Geert van Horrik <geert@catenalogic.com>
2026-05-14 23:45:07 +0200

    Update

--- src/CsvTextEditor/Views/SettingsWindow.xaml.cs
+++ src/CsvTextEditor/Views/SettingsWindow.xaml.cs
@@ -3,6 +3,9 @@
 
     public partial class SettingsWindow
     {
-
+        partial void OnInitializedComponent()
+        {
+            Mode = Catel.Windows.DataWindowMode.OkCancel;
+        }
     }
 }
